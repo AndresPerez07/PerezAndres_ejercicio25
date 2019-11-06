84b7217d9705ecf14f278d41ea6326383274c11c
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-05 23:21:52 -0500

    Add files via upload

--- clase25.cpp
+++ clase25.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
-int num_matriz(int *x);
-
+void relleno (float *matriz, int filas, int cols);
+void imprime (float *matriz, int filas, int cols);
+void imprimecabeza (float *matriz, int filas, int cols);
 int main() {
-  int **x;
+  int x;
 
   cout << "ingrese un numero del 3 al 11" << endl;
   cin >> x;
 
-  if (x > 3 && x < 11){
-
-  }
-  else {
+  if (x < 3 | x > 11){
     cout << "Esta fuera del rango" << x << endl;
   }
-
-  int cont = 0;
-  int ix = 0;
+  int filas = x+2;
+  int cols = x;
+  float *matriz = new float [filas*cols];
+  relleno(matriz,filas,cols);
+  imprime(matriz,filas,cols);
+  imprimecabeza(matriz,filas,cols);
 
 
   return 0;
+}
+void relleno (float *matriz, int filas, int cols){
+  for (size_t i = 0; i < filas; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      matriz[i*cols+j] = (i+1)+(j+1);
+    }
+  }
+}
+void imprime (float *matriz, int filas, int cols){
+  for (size_t i = 0; i < filas; i++) {
+    for (size_t j = 0; j < cols; j++) {
+
+      if (j==cols-1){
+        cout << " " << matriz[i*cols+j] << endl;
+      }
+      else {
+          cout << " " << matriz[i*cols+j];
+      }
+    }
+  }
+}
+void imprimecabeza (float *matriz, int filas, int cols){
+  cout << "la primera fila de la matriz es:" << endl;
+  for (size_t j = 0; j < cols; j++) {
+    cout << " " << matriz[0*cols + j];
+  }
+  float cont =0.0;
+  for (size_t i = 0; i < filas; i++) {
+    cont = cont + matriz[i*cols + 0];
+  }
+  cout << endl;
+  cout << "la suma de los valores de la primera columna es:" << endl;
+  cout << " " << cont << endl;
 }
